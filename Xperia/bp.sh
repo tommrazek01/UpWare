c099582f5af9e9d9455cd3aade1b2315ad39d1ed
Tom Mrazek <tomik98653200@gmail.com>
2013-02-16 21:56:26 +0400

    Base
    This is the base. I am looking for more xperia features to be added

---
+++ Xperia/bp.sh
@@ -0,0 +1,17 @@
+#!/sbin/sh
+busybox echo "ro.service.swiqi2.supported=true" >> /system/build.prop
+busybox echo "persist.service.swiqi2.enable=1" >> /system/build.prop
+busybox echo "ro.semc.sound_effects_enabled=true" >> /system/build.prop
+busybox echo "ro.semc.xloud.supported=true" >> /system/build.prop
+busybox echo "persist.service.xloud.enable=1" >> /system/build.prop
+busybox echo "ro.semc.enhance.supported=true" >> /system/build.prop
+busybox echo "persist.service.enhance.enable=1" >> /system/build.prop
+busybox echo "ro.semc.clearaudio.supported=true" >> /system/build.prop
+busybox echo "persist.service.clearaudio.enable=1" >> /system/build.prop
+busybox echo "ro.sony.walkman.logger=1" >> /system/build.prop
+busybox echo "persist.service.walkman.enable=1" >> /system/build.prop
+busybox echo "ro.somc.clearphase.supported=true" >> /system/build.prop
+busybox echo "persist.service.clearphase.enable=1" >> /system/build.prop
+busybox echo "af.resampler.quality=255" >> /system/build.prop
+busybox echo "persist.af.resampler.quality=255" >> /system/build.prop
+busybox echo "mpq.audio.decode=true" >> /system/build.prop
